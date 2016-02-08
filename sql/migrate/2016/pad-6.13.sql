4cf4f03fa9119b6078f7740acf7c9662a00659d5
dimitri <dvdveken@ovam.be>
2016-02-08 08:36:42 +0100

    rename dossier.dossier_id  --> dossier.dossier_nr

--- sql/migrate/2016/pad-6.13.sql
+++ sql/migrate/2016/pad-6.13.sql
@@ -110,6 +110,30 @@ left join art46.doelgroep_type doelgr on dos.doelgroep_type_id = doelgr.doelgroe
 ;
 
 
+DROP VIEW SMEG.ART46_DOSSIER_VIEW
+
+CREATE VIEW SMEG.ART46_DOSSIER_VIEW
+(
+   ID,
+   DOSSIER_ID,
+   DOSSIER_ID_BOA,
+   DOSSIER_B,
+   programma_type_b,
+   AMBTENAAR_NAAM,
+   AMBTENAAR_TEL
+)
+AS
+SELECT
+d.id,
+d.dossier_id,
+CAST(d.dossier_id_boa AS BIGINT) AS dossier_id_boa,
+d.dossier_b,
+coalesce(d.programma_code, '') programma_type_b,
+AMBTENAAR_B AMBTENAAR_NAAM,
+TEL1 AMBTENAAR_TEL
+FROM ART46.DOSSIER d left outer
+join ART46.OVAM_AMBTENAAR_VIEW oa on (oa.ambtenaar_id = d.doss_hdr_id)
+;
 
 
 delete from  art46.db_versie
@@ -251,6 +275,40 @@ drop view ART46.V_SCHULDVORDERING;create view art46.v_schuldvordering as select 
 drop view ART46.V_AANVR_SCHULDVORDERING;create view art46.v_aanvr_schuldvordering as     select         asv.aanvr_schuldvordering_id,         asv.offerte_id,         off.bestek_id,         off.bestek_nr,         off.bestek_omschrijving,         off.dossier_id,         off.dossier_nr,         off.dossier_type,         off.dossier_b,         off.dossier_b_l,         off.doss_hdr_id,         off.raamcontract_jn,         off.opdrachtgever_id,         off.opdrachtgever_naam_voornaam,         off.btw_tarief,         sv.vordering_id,         art46.schuldvordering_nr(sv.vordering_id) as schuldvordering_nr,         sv.deelopdracht_id,         do.deelopdracht_nr,         do.dossier_id as deelopdracht_dossier_id,         do.dossier_nr as deelopdracht_dossier_nr,         do.dossier_type as deelopdracht_dossier_type,         do.dossier_b as deelopdracht_dossier_b,         do.dossier_b_l as deelopdracht_dossier_b_l,         do.dossier_id_boa as deelopdracht_dossier_id_boa,         do.doss_hdr_id as deelopdracht_doss_hdr_id,         do.voorstel_deelopdracht_id,         do.voorstel_deelopdracht_nr,         sv.excl_btw_bedrag,         sv.vordering_bedrag,         sv.herziening_bedrag,         sv.boete_bedrag,         sv.goedkeuring_bedrag,         sv.vordering_correct_bedrag,         sv.herziening_correct_bedrag,         sv.van_d,         sv.tot_d,         sv.vordering_d,         sv.uiterste_verific_d,         sv.uiterste_d,         sv.goedkeuring_d,         sv.acceptatie_d,         sv.antwoord_pdf_brief_id,         antw_pdf_br.dms_id as antwoord_pdf_dms_id,         antw_pdf_br.dms_folder as antwoord_pdf_dms_folder,         antw_pdf_br.dms_filename as antwoord_pdf_dms_filename,         asv.beschrijving,         ( coalesce(sv.vordering_correct_bedrag, sv.vordering_bedrag, 0) + coalesce(sv.herziening_correct_bedrag, sv.herziening_bedrag, 0)) as totaal_incl_prijsherziening,         coalesce(sv.afgekeurd_jn,'N') as afgekeurd_jn,         asv.ingezonden_ts,         sv.schuldvordering_fase_id,         svf.schuldvordering_fase_b,         sv.commentaar,         sv.webloket_gebruiker_email,         sv.status,         st.name as status_pad,         st.name_web_loker as status_webloket,         asv.wijzig_ts     from art46.aanvr_schuldvordering as asv         inner join art46.v_offerte off on asv.offerte_id = off.offerte_id         left join art46.schuldvordering sv on asv.aanvr_schuldvordering_id = sv.aanvr_schuldvordering_id         left join art46.schuldvordering_fase svf on sv.schuldvordering_fase_id = svf.schuldvordering_fase_id         left join art46.schuldvordering_status st on sv.status = st.key         left join art46.v_deelopdracht do on sv.deelopdracht_id = do.deelopdracht_id         left join art46.brief antw_pdf_br on sv.antwoord_pdf_brief_id = antw_pdf_br.brief_id         left join art46.brief brief on sv.brief_id = antw_pdf_br.brief_id;
 
 
+
+
+drop view SMEG.ART46_DOSSIER_VIEW;
+
+CREATE VIEW SMEG.ART46_DOSSIER_VIEW
+(
+   ID,
+   DOSSIER_ID,
+   DOSSIER_ID_BOA,
+   DOSSIER_B,
+   programma_type_b,
+   AMBTENAAR_NAAM,
+   AMBTENAAR_TEL
+)
+AS
+SELECT
+d.id,
+d.dossier_nr,
+CAST(d.dossier_id_boa AS BIGINT) AS dossier_id_boa,
+d.dossier_b,
+coalesce(d.programma_code, '') programma_type_b,
+AMBTENAAR_B AMBTENAAR_NAAM,
+TEL1 AMBTENAAR_TEL
+FROM ART46.DOSSIER d left outer
+join ART46.OVAM_AMBTENAAR_VIEW oa on (oa.ambtenaar_id = d.doss_hdr_id)
+;
+
+
+select 'drop view ' || rtrim(v.VIEWSCHEMA) || '.' || rtrim(v.VIEWNAME) || ';' ||
+       replace(cast(v.text as varchar(32000)), chr(10) , ' ') || ';'
+from syscat.VIEWS v
+where v.viewschema = 'SMEG'
+     and v.valid = 'N'
+;
 
 
 -- deze versie van de wijzigingen in db registreren.
